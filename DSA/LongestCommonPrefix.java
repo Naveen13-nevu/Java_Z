59260dc336053d7a680e9e32530b0de1140ee1c5
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-09-18 22:32:37 +0530

    update

--- DSA/LongestCommonPrefix.java
+++ DSA/LongestCommonPrefix.java
@@ -1,3 +1,24 @@
 public class LongestCommonPrefix {
+
+    public static String longest(String []str){
+        
+        for(int i=0;i<str[0].length();i++){
+            char ch = str[0].charAt(i);
+            for(int j=1;j<str.length;j++){
+                if(i>=str[j].length() || str[j].charAt(i)!= ch){
+                    return str[0].substring(0,i);
+                }
+            }
+        }
+
+        return str[0];
+    }
     
+    public static void main(String [] args){
+
+        String [] str = {"flower", "flow","flag"};
+        String result = longest(str);
+        System.out.println(result);
+
+    }
 }
